40fba07197a7498d8236ab0bfbcf9e92a1c9035a
CARobotics619 <carobotics@gmail.com>
2015-08-30 16:54:26 -0400

    Update SwerveDriveBase.java

--- src/org/usfirst/frc/team619/robot/SwerveDriveBase.java
+++ src/org/usfirst/frc/team619/robot/SwerveDriveBase.java
@@ -11,12 +11,6 @@ public class SwerveDriveBase {
     
 	private TalonCan leftMotor, rightMotor, leftMotor2, rightMotor2;
 	private Talon leftTurn, rightTurn, leftTurn2, rightTurn2;
-
-    public SwerveDriveBase(int leftMotorTalonID, int rightMotorTalonID) {
-        leftMotor = new TalonCan(leftMotorTalonID);
-        rightMotor = new TalonCan(rightMotorTalonID);
-        rightMotor.setReversed(true);
-    }
     
     public SwerveDriveBase(int leftMotorTalonID, int rightMotorTalonID, int leftMotorTalonID2, int rightMotorTalonID2, int leftTurnTalonID, int rightTurnTalonID, int leftTurnTalonID2, int rightTurnTalonID2){
         leftMotor = new TalonCan(leftMotorTalonID);
@@ -29,23 +23,23 @@ public class SwerveDriveBase {
         rightTurn2 = new Talon(rightTurnTalonID2);
     }//end constructor TalonDriveBase
     
-    public SwerveDriveBase(TalonCan leftMotor, TalonCan rightMotor, Talon leftTurn, Talon rightTurn){
+    public SwerveDriveBase(TalonCan leftMotor, TalonCan rightMotor, TalonCan leftMotor2, TalonCan rightMotor2){
         this.leftMotor = leftMotor;
-        this.leftTurn = leftTurn;
+        this.leftMotor2 = leftMotor2;
         this.rightMotor = rightMotor;
-        this.rightTurn = rightTurn;
+        this.rightMotor2 = rightMotor2;
         rightMotor.setReversed(true);
     }//end constructor TalonDriveBase
 
-    public SwerveDriveBase(TalonCan leftMotor1, TalonCan leftMotor2, TalonCan rightMotor1, TalonCan rightMotor2, Talon rightMotorTurn1, Talon rightMotorTurn2, Talon leftMotorTurn1, Talon leftMotorTurn2) {
-        this.leftMotor = leftMotor1;
-        this.leftMotor = leftMotor2;
-        this.rightMotor = rightMotor1;
-        this.rightMotor = rightMotor2;
-        this.leftTurn = leftMotorTurn1;
-        this.leftTurn = leftMotorTurn2;
-        this.rightTurn = rightMotorTurn1;
-        this.rightTurn = rightMotorTurn2;
+    public SwerveDriveBase(TalonCan leftMotor, TalonCan leftMotor2, TalonCan rightMotor, TalonCan rightMotor2, Talon rightMotorTurn, Talon rightMotorTurn2, Talon leftMotorTurn, Talon leftMotorTurn2) {
+        this.leftMotor = leftMotor;
+        this.leftMotor2 = leftMotor2;
+        this.rightMotor = rightMotor;
+        this.rightMotor2 = rightMotor2;
+        this.leftTurn = leftMotorTurn;
+        this.leftTurn2 = leftMotorTurn2;
+        this.rightTurn = rightMotorTurn;
+        this.rightTurn2 = rightMotorTurn2;
     }
 
     protected SwerveDriveBase() {
@@ -61,32 +55,50 @@ public class SwerveDriveBase {
         return rightMotor;
     }
     
-    public Talon getLeftTalon2(){
-        return leftTurn;
+    public TalonCan getLeftTalon2(){
+        return leftMotor2;
     }//end Talon getLeftTalon2
     
-    public Talon getRightTalon2(){
-        return rightTurn;
+    public TalonCan getRightTalon2(){
+        return rightMotor2;
     }//end Talon getRightTalon2
-
-    public void drive(double leftPercent, double rightPercent) {
-        leftMotor.set(leftPercent);
-        rightMotor.set(rightPercent);
+    
+    public Talon getLeftTurn(){
+    	return leftTurn;
     }
+    
+    public Talon getRightTurn(){
+    	return rightTurn;
+    }
+    
+    public Talon getLeftTurn2(){
+    	return leftTurn2;
+    }
+    
+    public Talon getRightTurn2(){
+    	return rightTurn2;
+    }
+
     
     public void drive(double percent){
     	leftMotor.set(percent);
-    	rightMotor.set(percent);
+    	leftMotor2.set(percent);
+    	rightMotor.set(-percent);
+    	rightMotor2.set(-percent);
     }
     
     public void turn(double turnPercent /*positive number goes right negative number goes left*/){
-    	leftMotor.set(turnPercent);
-    	rightMotor.set(-turnPercent);
+    	leftTurn.set(turnPercent);
+    	leftTurn2.set(turnPercent);
+    	rightTurn.set(-turnPercent);
+    	rightTurn2.set(-turnPercent);
     }
     
     public void stop(){
     	leftMotor.set(0);
     	rightMotor.set(0);
+    	leftTurn.set(0);
+    	rightTurn.set(0);
     }
     
 }
